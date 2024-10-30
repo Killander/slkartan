9ea3511d1bf4e63a49ededdebee84e8f8fed2c72
Killander <chriskillander@gmail.com>
2024-10-30 17:34:16 +0100

    cleanup

--- public/mapscript.js
+++ public/mapscript.js
@@ -57,6 +57,64 @@ export function init() {
     }
 }
 
+export function getVehicles() {
+    let interval;
+    fetch("https://opendata.samtrafiken.se/gtfs-rt/sl/VehiclePositions.pb?key=" + trafiklab_api_key)
+        .then(response => {
+            if (!response.ok) {
+                throw response;
+            }
+            return response.arrayBuffer();
+        })
+        .then(data => {
+            const pbf = new Pbf(new Uint8Array(data));
+            const obj = FeedMessage.read(pbf);
+            return obj.entity;
+        })
+        .then(data => data.filter(x => !!x.vehicle))
+        .then(data => data.filter(x => !!x.vehicle.trip))
+        .then(data => enrichVehicles(data))
+        .then(data => addVehicles(data))
+        .catch(error => {
+            console.error('There was a problem fetching vehicles:', error);
+            clearInterval(interval);
+            const five_minutes = 5 * 60 * 1000;
+            console.log('Retrying fetch in 5 minutes');
+            delay(five_minutes).then(() => interval = setInterval(getVehicles, 2000));
+        });
+}
+
+function enrichVehicles(data) {
+    return data.map(entity => {
+        let trip = tripMap.get(entity.vehicle.trip.trip_id);
+        if (trip != null) {
+            let route = routeMap.get(trip.route_id);
+            entity.vehicle.vehicle.label = route.route_short_name;
+            entity.vehicle.vehicle.type = route.route_type;
+        }
+        return entity;
+    });
+}
+
+function addVehicles(data) {
+    data.forEach(entity => {
+        let type = entity.vehicle.vehicle.type;
+        if (type === utils.TRANSPORT.TRAIN) {
+            addVehicle(entity, marker_id_map_train, layer_train);
+        } else if (type === utils.TRANSPORT.METRO) {
+            addVehicle(entity, marker_id_map_metro, layer_metro);
+        } else if (type === utils.TRANSPORT.BUS) {
+            addVehicle(entity, marker_id_map_bus, layer_bus);
+        } else if (type === utils.TRANSPORT.TRAM) {
+            addVehicle(entity, marker_id_map_tram, layer_tram);
+        } else if (type === utils.TRANSPORT.VESSEL) {
+            addVehicle(entity, marker_id_map_vessel, layer_vessel);
+        } else {
+            addVehicle(entity, marker_id_map_unknown, layer_unknown);
+        }
+    });
+}
+
 function addVehicle(entity, marker_id_map, layer) {
     let vehicle = entity.vehicle;
     let id = vehicle.vehicle.id;
@@ -94,64 +152,6 @@ function addVehicle(entity, marker_id_map, layer) {
     }
 }
 
-export function getVehicles() {
-    let interval;
-    fetch("https://opendata.samtrafiken.se/gtfs-rt/sl/VehiclePositions.pb?key=" + trafiklab_api_key)
-        .then(response => {
-            if (!response.ok) {
-                throw response;
-            }
-            return response.arrayBuffer();
-        })
-        .then(data => {
-            const pbf = new Pbf(new Uint8Array(data));
-            const obj = FeedMessage.read(pbf);
-            return obj.entity;
-        })
-        .then(data => data.filter(x => !!x.vehicle))
-        .then(data => data.filter(x => !!x.vehicle.trip))
-        .then(data => enrichVehicles(data))
-        .then(data => addVehicles(data))
-        .catch(error => {
-            console.error('There was a problem fetching vehicles:', error);
-            clearInterval(interval);
-            const five_minutes = 5 * 60 * 1000;
-            console.log('Retrying fetch in 5 minutes');
-            delay(five_minutes).then(() => interval = setInterval(getVehicles, 2000));
-        });
-}
-
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
-}
-
-function enrichVehicles(data) {
-    return data.map(entity => {
-        let trip = tripMap.get(entity.vehicle.trip.trip_id);
-        if (trip != null) {
-            let route = routeMap.get(trip.route_id);
-            entity.vehicle.vehicle.label = route.route_short_name;
-            entity.vehicle.vehicle.type = route.route_type;
-        }
-        return entity;
-    });
-}
-
-function addVehicles(data) {
-    data.forEach(entity => {
-        let type = entity.vehicle.vehicle.type;
-        if (type === utils.TRANSPORT.TRAIN) {
-            addVehicle(entity, marker_id_map_train, layer_train);
-        } else if (type === utils.TRANSPORT.METRO) {
-            addVehicle(entity, marker_id_map_metro, layer_metro);
-        } else if (type === utils.TRANSPORT.BUS) {
-            addVehicle(entity, marker_id_map_bus, layer_bus);
-        } else if (type === utils.TRANSPORT.TRAM) {
-            addVehicle(entity, marker_id_map_tram, layer_tram);
-        } else if (type === utils.TRANSPORT.VESSEL) {
-            addVehicle(entity, marker_id_map_vessel, layer_vessel);
-        } else {
-            addVehicle(entity, marker_id_map_unknown, layer_unknown);
-        }
-    });
 }
